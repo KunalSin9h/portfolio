2b27c5fda79bfcbb7230b280e3b6a63af94afe36
Kunal Singh <kunalsin9h@gmail.com>
2022-12-03 10:11:04 +0530

    remove api

--- src/pages/api/ping.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function (req: NextApiRequest, res: NextApiResponse) {
-  res.redirect(307, "https://kunalsin9h.dev");
-}
